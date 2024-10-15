2066346f8e75778a5cf57f63cd05833df04e4259
malakbenothmen <malekbenothmen70@gmail.com>
2024-10-15 13:10:52 +0200

    Spirint 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,44 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoyagesComponent } from './voyages/voyages.component';
+import { AddVoyageComponent } from './add-voyage/add-voyage.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateVoyageComponent } from './update-voyage/update-voyage.component';
+import { HttpClientModule  } from '@angular/common/http';
+import { RechercheParTypeComponent } from './recherche-par-type/recherche-par-type.component';
+import { RechercheParDestinationComponent } from './recherche-par-destination/recherche-par-destination.component';
+import { SearchFilterPipe } from './search-filter.pipe';
+import { ListeTypesComponent } from './liste-types/liste-types.component';
+import { UpdateTypeComponent } from './update-type/update-type.component';
+import { LoginComponent } from './login/login.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoyagesComponent,
+    AddVoyageComponent,
+    UpdateVoyageComponent,
+    RechercheParTypeComponent,
+    RechercheParDestinationComponent,
+    SearchFilterPipe,
+    ListeTypesComponent,
+    UpdateTypeComponent,
+    LoginComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+   
   ],
   bootstrap: [AppComponent]
 })
